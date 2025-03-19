0762a62c85f6018033409aebcabecb2c02a6eb69
Antoine Fourcade <afourcade@dev-id.fr>
2025-03-19 23:14:10 +0100

    banger de config

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -1,23 +1,11 @@
-local noice = require 'noice'
-
-noice.setup {
-  lsp = {
-    override = {
-      ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-      ['vim.lsp.util.stylize_markdown'] = true,
-    },
-    hover = {
-      enable = true,
-      silent = false,
-    },
+return {
+  'folke/noice.nvim',
+  event = 'VeryLazy',
+  opts = {
+    -- add any options here
   },
-  presets = {
-    bottom_search = true,
-    command_palette = true,
-    long_message_to_split = true,
-    inc_rename = true,
-    lsp_doc_border = false,
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    'MunifTanjim/nui.nvim',
   },
 }
-
-return {}
